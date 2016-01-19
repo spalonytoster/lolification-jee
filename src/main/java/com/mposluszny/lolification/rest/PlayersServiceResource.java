53f45088aaee50701f91082696f5439542df7e89
Maciej Posłuszny <xmachiei@gmail.com>
2016-01-20 00:39:09 +0100

    end of the day

--- src/main/java/com/mposluszny/lolification/rest/PlayersServiceResource.java
+++ src/main/java/com/mposluszny/lolification/rest/PlayersServiceResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -22,5 +23,12 @@ public class PlayersServiceResource {
 	@Produces(value=MediaType.APPLICATION_JSON)
 	public List<Player> getAllPlayers() {
 		return playerDao.getAllPlayers();
+	}
+	
+	@GET
+	@Path("/player/{idPlayer}")
+	@Produces(value=MediaType.APPLICATION_JSON)
+	public Player getPlayerById(@PathParam("idPlayer") long idPlayer) {
+		return playerDao.getPlayerById(idPlayer);
 	}
 }
--- src/main/java/com/mposluszny/lolification/rest/TeamsServiceResource.java
+++ src/main/java/com/mposluszny/lolification/rest/TeamsServiceResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -26,6 +27,15 @@ public class TeamsServiceResource {
 			team.setPlayers(teamDao.getPlayersForTeam(team));
 		}
 		return teams;
+	}
+	
+	@GET
+	@Path("/team/{idTeam}")
+	@Produces(value=MediaType.APPLICATION_JSON)
+	public Team getTeamById(@PathParam("idTeam") long idTeam) {
+		Team team = teamDao.getTeamById(idTeam);
+		team.setPlayers(teamDao.getPlayersForTeam(team));
+		return team;
 	}
 
 }
